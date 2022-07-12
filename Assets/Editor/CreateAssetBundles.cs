24120a48722c6aaf87487d94bff7518d44e958d1
Vivek Nair <vivek@nair.me>
2022-07-11 17:07:05 -0700

    add prefab

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,9 @@
+using UnityEditor;
+
+public class CreateAssetBundles {
+
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles() {
+		BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+	}
+}
